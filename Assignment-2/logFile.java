9c45ee57125e2a37bfb1914d8c5a7f6dfded3b98
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-03-25 18:44:05 +0530

    Add files via upload

--- Assignment-2/logFile.java
+++ Assignment-2/logFile.java
@@ -1,96 +1,109 @@
-package Demo;
+package la;
+
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.Path;
+
 import org.apache.hadoop.io.IntWritable;
+
 import org.apache.hadoop.io.LongWritable;
+
 import org.apache.hadoop.io.Text;
+
 import org.apache.hadoop.mapreduce.Job;
+
 import org.apache.hadoop.mapreduce.Mapper;
+
 import org.apache.hadoop.mapreduce.Reducer;
+
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class WordCount {
+public class Demo {
 
-public static void main(String [] args) throws Exception
+	// Main
+	public static void main(String[] args) throws Exception
 
-{
+	{
 
-Configuration c=new Configuration();
+		Configuration c = new Configuration();
 
+		String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
 
+		Path input = new Path(files[0]);
 
-Path input=new Path(args[0]);
+		Path output = new Path(files[1]);
 
-Path output=new Path(args[1]);
+		Job j = new Job(c, "logCounter");
 
-Job j=new Job(c,"wordcount");
+		j.setJarByClass(Demo.class);
 
-j.setJarByClass(WordCount.class);
+		j.setMapperClass(MapForLogCount.class);
 
-j.setMapperClass(MapForWordCount.class);
+		j.setReducerClass(ReduceForLogCount.class);
 
-j.setReducerClass(ReduceForWordCount.class);
+		j.setOutputKeyClass(Text.class);
 
-j.setOutputKeyClass(Text.class);
+		j.setOutputValueClass(IntWritable.class);
 
-j.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(j, input);
 
-FileInputFormat.addInputPath(j, input);
+		FileOutputFormat.setOutputPath(j, output);
 
-FileOutputFormat.setOutputPath(j, output);
+		System.exit(j.waitForCompletion(true) ? 0 : 1);
 
-System.exit(j.waitForCompletion(true)?0:1);
+	}
 
-}
+	public static class MapForLogCount extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable>{
+		public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
 
-public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
+		{
 
-{
+			String line = value.toString();
 
-String line = value.toString();
+			String[] words = line.split(",");
 
-String[] words=line.split(",");
+			String Ip = words[1];
 
-  Text outputKey = new Text(words[0].toUpperCase().trim());
+			Text outputKey = new Text(Ip);
 
-  int min = Integer.parseInt(words[1]);
-  int max = Integer.parseInt(words[2]);
+			IntWritable outputValue = new IntWritable(1);
 
-  int val = max - min;
-  IntWritable outputValue = new IntWritable(val);
+			con.write(outputKey, outputValue);
 
-  con.write(outputKey, outputValue);
+		}
 
-}
-}
-public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable>
+	}
 
-{
+	public static class ReduceForLogCount extends Reducer<Text, IntWritable, Text, IntWritable>
 
-public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException
+	{
 
-{
+		public void reduce(Text word, Iterable<IntWritable> values, Context con)
+				throws IOException, InterruptedException
 
-int sum = 0;
+		{
 
-   for(IntWritable value : values)
+			int sum = 0;
 
-   {
+			for (IntWritable value : values)
 
-   sum += value.get();
+			{
 
-   }
+				sum += value.get();
 
-   con.write(word, new IntWritable(sum));
+			}
 
-}
+			con.write(word, new IntWritable(sum));
 
-}
+		}
+
+	}
 
 }
--- Assignment-3/Weather.java
+++ Assignment-3/Weather.java
@@ -1,114 +1,96 @@
-package Demo;
-import java.io.IOException;
+package maxWeather;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Wheather {
-
-public static void main(String [] args) throws Exception
-
-{
-
-Configuration c=new Configuration();
-
-
-
-Path input=new Path(args[0]);
-
-Path output=new Path(args[1]);
-
-Job j=new Job(c,"wordcount");
-
-j.setJarByClass(Wheather.class);
-
-j.setMapperClass(MapForWordCount.class);
-
-j.setReducerClass(ReduceForWordCount.class);
-
-j.setOutputKeyClass(Text.class);
-
-j.setOutputValueClass(IntWritable.class);
-
-FileInputFormat.addInputPath(j, input);
-
-FileOutputFormat.setOutputPath(j, output);
-
-System.exit(j.waitForCompletion(true)?0:1);
-
-}
-
-public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable>{
-
-public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
-
-{
-
-String line = value.toString();
-int temp = 0;
-String temperature = line.substring(87,92);
-String year = line.substring(15,19);
-temp = Integer.parseInt(temperature);
-Text outputKey = new Text(year);
-
-IntWritable outputValue = new IntWritable(temp);
-
-con.write(outputKey, outputValue);
-
-
-}
-}
-public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable>
-
-{
-	Text maxWord = new Text();
-	Text minWord = new Text();
-
-	int min = 9999;
-	int max = 0;
-
-public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException
-
-{
-
-int sum = 0;
-int count = 0;
-for(IntWritable value : values)
-
-   {
-
-   sum += value.get();
-   count++;
-   }
-
-   int avg = sum / count;
-   if(max<=avg) {
-	   max = avg;
-	   maxWord.set(word);
-   }
-
-   if(min >= avg) {
-	   min = avg;
-	   minWord.set(word);
-   }
-}
-@Override
-	protected void cleanup(Reducer<Text, IntWritable, Text, IntWritable>.Context context)
-			throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-	   context.write(maxWord, new IntWritable(max));
-	   context.write(minWord, new IntWritable(min));
-
-	super.cleanup(context);
+public class MaxTemperature {
+	public static class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private static final int MISSING = 9999;
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			String year = line.substring(15, 19);
+			int airTemperature;
+			if(line.charAt(87) == '+') {
+				airTemperature = Integer.parseInt(line.substring(88,92));
+			}
+			else {
+				airTemperature = Integer.parseInt(line.substring(87,92));
+			}
+			String quality = line.substring(92,93);
+			if(airTemperature != MISSING && quality.matches("[01459]")) {
+				context.write(new Text(year), new IntWritable(airTemperature));
+			}
+		}
 	}
-}
 
+	public static class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		static int max = 0, min = 0;
+		Text maxYear, minYear;
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int avgValue = 0;
+			int count = 0;
+			String year = key.toString();
+			for(IntWritable value : values) {
+				avgValue += value.get();
+				count++;
+			}
+			avgValue = avgValue / count;
+			if(year.equals("1901") == true) {
+				max = avgValue;
+				min = avgValue;
+			}
+			try {
+				if(avgValue > max) {
+					max = avgValue;
+					maxYear.set(key.getBytes());
+				}
+				else if(avgValue < min) {
+					min = avgValue;
+					minYear.set(key.getBytes());
+				}
+			}
+			catch(NullPointerException ex) {
+
+			}
+
+			if(year.equals("1920") == true) {
+				//System.out.println("MAX");
+				Text maxLabel = new Text("MAXIMUM TEMPERATURE ");
+				IntWritable maxTemp = new IntWritable(max);
+				Text minLabel = new Text("MINIMUM TEMPERATURE  ");
+				IntWritable minTemp = new IntWritable(min);
+				context.write(maxLabel, maxTemp);
+				context.write(minLabel, minTemp);
+				//System.out.println("MIN");
+				//context.write(minYear, new IntWritable(min));
+			}
+ 		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		if(args.length != 2) {
+			System.err.println("Usage: MaxTemperature <input path> <output path>");
+			System.exit(-1);
+		}
+		Job job = new Job();
+		job.setJarByClass(MaxTemperature.class);
+		job.setJobName("Max Temperature");
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapperClass(MaxTemperatureMapper.class);
+		job.setReducerClass(MaxTemperatureReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
